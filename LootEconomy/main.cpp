72f79a4f7962c2efaa1f05f32db176cfcadcba1c
Maciej Mróz <maciek@MaciekMBP.local>
2012-12-21 12:57:54 +0100

    Initial Commit

---
+++ LootEconomy/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LootEconomy
+//
+//  Created by Maciej Mróz on 21.12.2012.
+//  Copyright (c) 2012 Maciej Mróz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
